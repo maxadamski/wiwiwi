97d41b65806525155cf1ba6941b6551b6b1b2ba2
Max Adamski <maxadamski@gmx.com>
2019-03-31 00:52:26 +0100

    more shit

--- semestr_4/ask/3_timer.asm
+++ semestr_4/ask/3_timer.asm
@@ -10,45 +10,43 @@ jmp  inter
 start:
 mov  ie,   #10000010b
 mov  tmod, #00000001b
-mov  tl0,  50000   ; czy to jest ok?
-mov  th0,  15536   ; to samo
+mov  th0,  #high 15356
+mov  tl0,  #low  50000
 setb tr0
-mov  r0, #0
-mov  r1, #0
-mov  r2, #0
-mov  r3, #0
+mov  r0,   #00000000b
+mov  r1,   #00010000b
+mov  r2,   #01100000b
+mov  r3,   #00110000b
 
 displ:
-mov  a,  #00000000b ; wyświetlacz 0
-add  a,  r0
-mov  p2, a
-mov  a,  #00010000b ; wyświetlacz 1
-add  a,  r1
-mov  p2, a
-mov  a,  #01100000b ; wyświetlacz 2 + kropka
-add  a,  r2
-mov  p2, a
-mov  a,  #00110000b ; wyświetlacz 3
-add  a,  r3
-mov  p2, 
+mov  p2, r0
+call delay
+mov  p2, r1
+call delay
+mov  p2, r2
+call delay
+mov  p2, r3
+call delay
 jmp  displ
 
 inter:
+djnz r4, skip3  ; czekamy 40 przerwań
+mov  r4, #40
 inc  r0
-cjne r0, #1010b, skip0
-mov  r0, #0 ; resetuj jeśli było 9
+cjne r0, #00001010b, skip0
+mov  r0, #00000000b ; resetuj jeśli było 9
 skip0:
 inc  r1
-cjne r1, #0111b, skip1
-mov  r1, #0 ; resetuj jeśli było 6
+cjne r1, #00010111b, skip1
+mov  r1, #00010000b ; resetuj jeśli było 6
 skip1:
 inc  r2
-cjne r2, #1010b, skip2
-mov  r2, #0 ; resetuj jeśli było 9
+cjne r2, #01101010b, skip2
+mov  r2, #01100000b ; resetuj jeśli było 9
 skip2:
 inc  r3
-cjne r3, #0111b, skip1
-mov  r3, #0 ; resetuj jeśli było 6
+cjne r3, #00000111b, skip1
+mov  r3, #00110000b ; resetuj jeśli było 6
 skip3:
 reti
 
